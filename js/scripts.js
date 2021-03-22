1a4f99e7ed95975d8202e8fa9a00a2ff0b338582
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-03-22 15:51:11 +0100

    add dice roller

--- js/scripts.js
+++ js/scripts.js
@@ -35,5 +35,24 @@ if(i >= emailList.length) {
 Tasks 
 1 - Generare numero random da 1 a 6 per l'utente
 2 - Generare numero random da 1 a 6 per il computer
-3 - 
+3 - Comparare i numeri
 */
+
+//1
+var userNumber = Math.floor(Math.random() * 6 + 1);
+//console.log(userNumber);
+document.getElementById("user-number").innerHTML = userNumber;
+
+//2
+var pcNumber = Math.floor(Math.random() * 6 + 1);
+//console.log(pcNumber);
+document.getElementById("pc-number").innerHTML = pcNumber;
+
+//3
+if(pcNumber > userNumber) {
+    document.getElementById("result").innerHTML = "Hai Perso :(";
+} else if(pcNumber < userNumber) {
+    document.getElementById("result").innerHTML = "Hai Vinto! :)";
+} else {
+    document.getElementById("result").innerHTML = "Pari! :O"
+}
